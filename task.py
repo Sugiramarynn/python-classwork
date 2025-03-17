47449c842502bd859408c4aaebcdb375040c8e7c
sugiramarynn <sugiramarylene@gmail.com>
2025-03-17 22:24:03 +0400

    Third Version

--- task.py
+++ task.py
@@ -3,6 +3,14 @@
 # print(f"Your name has {character_count} characters.")
 # print("hello")
 
-text = input("enter your full name")
-character = len(text.replace(" ",""))
-print(f"Characters are ={character}")
+# text = input("enter your full name")
+# character = len(text.replace(" ",""))
+# print(f"Characters are ={character}")
+
+words=[]
+for i in range(1,6):
+     word = input(f"Enter your full names {i}:")
+     words.append(word)
+     print("\n Word Character:")
+     for word in words:
+          print(f"'{word}' has {len(word)}characters")
