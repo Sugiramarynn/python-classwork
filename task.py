41a95700ae448d417ace097f2512309deca12167
sugiramarynn <sugiramarylene@gmail.com>
2025-03-17 22:13:16 +0400

    Second version

--- task.py
+++ task.py
@@ -1,4 +1,8 @@
-Text = input("Enter your full names: ")
-character_count = len(Text)
-print(f"Your name has {character_count} characters.")
-print("hello")
+# Text = input("Enter your full names: ")
+# character_count = len(Text)
+# print(f"Your name has {character_count} characters.")
+# print("hello")
+
+text = input("enter your full name")
+character = len(text.replace(" ",""))
+print(f"Characters are ={character}")
